d24eadc39ffacbc1c6cde4242fddf1222360914d
Tim Siers <tim@oxideinteractive.com.au>
2018-05-10 17:30:56 +1000

    Backgrounds and accessibility changes

--- dfata_theme/dist/js/script.js
+++ dfata_theme/dist/js/script.js
@@ -1,4 +1,21 @@
 /**
+ * 
+ * For accessibility wrap home page tiles in list and list roles
+ *
+ */
+(function($, Drupal, window, document, undefined) {
+
+  $(document).ready(function() {
+    $(".front .panels-flexible-region-4-landing_page_tiles-inside").attr("role", "list");
+    $(".front .panels-flexible-region-4-landing_page_tiles-inside > div.panel-pane").each(function() {
+      $(this).attr("role", "listitem");
+    });
+  });
+
+})(jQuery, Drupal, this, this.document);
+
+
+/**
  * Accordion behaviour.
  */
 (function($, Drupal, window, document, undefined) {
